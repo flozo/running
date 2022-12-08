55abc7099f1692a0de79deeef9024a351f79bac3
flozo.home <github.mail@flozo.de>
2022-12-08 16:34:18 +0100

    Remove unused method from NotFoundException

--- data/src/main/java/de/flozo/running/exceptions/NotFoundException.java
+++ data/src/main/java/de/flozo/running/exceptions/NotFoundException.java
@@ -14,8 +14,4 @@ public class NotFoundException extends RuntimeException {
         super(message);
     }
 
-    public NotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
 }

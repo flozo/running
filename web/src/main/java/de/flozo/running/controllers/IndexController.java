d1314d695420c8ec3981f167f328f2ef5c6851bd
flozo.home <github.mail@flozo.de>
2022-12-08 17:28:54 +0100

    Add IndexController

---
+++ web/src/main/java/de/flozo/running/controllers/IndexController.java
@@ -0,0 +1,22 @@
+package de.flozo.running.controllers;
+
+import de.flozo.running.services.RunningEventService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    private final RunningEventService runningEventService;
+
+    public IndexController(RunningEventService runningEventService) {
+        this.runningEventService = runningEventService;
+    }
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getIndexPage(Model model) {
+        model.addAttribute("running_events", runningEventService.findAll());
+        return "index";
+    }
+}

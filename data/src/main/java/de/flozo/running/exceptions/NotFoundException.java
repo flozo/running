7b8c90e30bc5146f8a39ef4f371c78b5b3ef4a4c
flozo.home <github.mail@flozo.de>
2022-12-08 17:28:14 +0100

    Add bootstrap and jquery webjars to Maven dependencies

--- data/src/main/java/de/flozo/running/exceptions/NotFoundException.java
+++ data/src/main/java/de/flozo/running/exceptions/NotFoundException.java
@@ -1,9 +1,6 @@
 package de.flozo.running.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
+//@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException() {

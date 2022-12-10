e49c08c32c568ed27626dea91795c7d740df2697
flozo.home <github.mail@flozo.de>
2022-12-10 20:15:44 +0100

    Use prefix for @RequestMapping in controllers

--- web/src/main/java/de/flozo/running/controllers/RunningEventController.java
+++ web/src/main/java/de/flozo/running/controllers/RunningEventController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/running_event")
 @Controller
 public class RunningEventController {
 
@@ -22,20 +24,19 @@ public class RunningEventController {
     }
 
 
-    @GetMapping("/running_event/{id}/show")
+    @GetMapping("/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("running_event", runningEventService.findById(Long.valueOf(id)));
         model.addAttribute("laps", lapService.findAllByRunningEventIdOrderByLapNumberAsc(Long.valueOf(id)));
         return "running_event/show";
     }
 
-    @GetMapping("/running_event/{id}/update")
+    @GetMapping("/{id}/update")
     public String updateRunningEvent(@PathVariable String id, Model model) {
         model.addAttribute("running_event", runningEventService.findById(Long.valueOf(id)));
         model.addAttribute("laps", lapService.findAllByRunningEventIdOrderByLapNumberAsc(Long.valueOf(id)));
         model.addAttribute("routes", routeService.findAll());
         return "running_event/runningEventForm";
     }
-
 
 }

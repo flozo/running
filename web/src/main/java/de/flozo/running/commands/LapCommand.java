358bed5d7e1bb2edcd8f19cb9295611ead535573
flozo.home <github.mail@flozo.de>
2023-01-27 19:22:21 +0100

    Add command layer; add LapCommand

---
+++ web/src/main/java/de/flozo/running/commands/LapCommand.java
@@ -0,0 +1,36 @@
+package de.flozo.running.commands;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalTime;
+
+public class LapCommand {
+
+    @NotBlank
+    @Min(1)
+    @Max(1000)
+    private Integer lapNumber;
+
+
+    @NotBlank
+    private LocalTime lapTime;
+
+    @Min(30)
+    @Max(300)
+    private Integer avgHeartRate;
+
+    @Min(30)
+    @Max(300)
+    private Integer maxHeartRate;
+
+    private Double energyBurnedValue;
+
+    @NotNull
+    @Min(1)
+    private Long energyBurnedUnitId;
+
+
+}
